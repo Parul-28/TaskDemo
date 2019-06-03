1f008e2f6eb915d073763fa983de3d6cf0f48ce2
Parul <parul.281997@gmail.com>
2019-06-03 23:34:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/taskdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.taskdemo;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Okhla Industrial Area");
+        toolbar.setSubtitle("New Delhi");
+
+    }
+
+}
